7b2052c8d1f4c0d9bd50e3b2d6c6912716e481eb
mrehmm001 <muneeb73@outlook.com>
2022-05-30 22:01:07 +0100

    minor fix

--- src/components/errorPrompt/ErrorPromp.jsx
+++ src/components/errorPrompt/ErrorPromp.jsx
@@ -3,7 +3,7 @@ function ErrorPrompt() {
     return ( 
         <div className="errorParent">
             <div className="subHeading">Errors</div>
-            Please make sure all supplied values and numerical and are between 1 and 100.
+            Please make sure all supplied values are between 1 and 100.
         </div>
      );
 }
