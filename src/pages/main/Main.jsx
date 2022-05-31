e403cc5451f519bbdd7c06383d54d3f2f1b40a07
mrehmm001 <muneeb73@outlook.com>
2022-05-31 18:01:14 +0100

    Minor addition

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -151,7 +151,7 @@ function Main() {
                 </div>
 
                 <div className="breakdown">
-                According to <b><a href="https://www.gold.ac.uk/media/docs/gam/Progression-and-Award-for-Students-on-Taught-Programmes.pdf" className="link">rogression and Award for
+                According to <b><a href="https://www.gold.ac.uk/media/docs/gam/Progression-and-Award-for-Students-on-Taught-Programmes.pdf" className="link">Progression and Award for
 Students on Taught Programmes 9.3.6</a></b> you will automatically be  awarded the higher classification:<br/>
             <b>*{finalGrade} = {classificationFunction(Math.max(weightedTotal1,weightedTotal2))}</b>
                 </div>
