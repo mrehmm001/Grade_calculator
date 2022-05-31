b320518eda0056195a54e27ce43fe18b79700515
mrehmm001 <muneeb73@outlook.com>
2022-05-31 23:25:00 +0100

    Minor fix

--- src/components/result/Result.jsx
+++ src/components/result/Result.jsx
@@ -3,9 +3,7 @@ function Result({result, breakdown, classification}) {
     return ( <div className="resultParent">
         <div className="top">
             <div className="subHeading">Results</div>
-            <ul>
-                <li>Your final grade is a <b>{classification}</b> (with a final weighted average mark of <b>{result}%)</b></li>
-            </ul>
+            Your final grade is a <b>{classification}</b> (with a final weighted average mark of <b>{result}%</b>)
         </div>
         <div className="bottom">
         <div className="subHeading">Breakdown</div>
